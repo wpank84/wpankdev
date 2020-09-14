5018598ca80209a5591870a45ae8b8f29a14cf37
wpank <will@celosiadesigns.com>
2020-09-14 13:00:45 -0500

    changed db to postgres

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_09_13_190309) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "paragraphs", force: :cascade do |t|
     t.string "content"
     t.integer "project_id", null: false
